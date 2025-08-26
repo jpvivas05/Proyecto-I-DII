fa3cc62bf651e3001b1b5b9d2c0373d945f308a6
jpvivas05 <viv23105@uvg.edu.gt>
2025-08-26 17:50:00 -0600

    Update init.h

--- init.h
+++ init.h
@@ -1,6 +1,6 @@
 #ifndef __INIT_H__
 #define __INIT_H__
-
+//  Incluir mismas librerías que en main
 #include <Arduino.h>
 #include <stdint.h>
 #include "driver/ledc.h"
@@ -21,31 +21,33 @@
 #define LEDv 33
 #define LEDa 25
 #define Servo 13
-//  Pre configuración PWM
+//  Pre configuración de canales y especificaciones para PWM
+//  Canales utilizados
 #define canalR 0
 #define canalV 1
 #define canalA 4
 #define canalServo 3
-
+//  Frecuencia definida
 #define freqPWM 100
 #define freqServo 50
-
+//  Resolución recomendada
 #define resPWM 10
 #define resServo 12
-//  Definición de sensor y botón
+
+//  Definición de GPIO sensor y botón
 #define btn1 23
 #define temp 36
-
+//  Debounce para botón
 #define delayBounce 250
-
+//  Ciclo para display
 #define delayDisplay 5
 
+//      Prototipos de funciones
 //  Funciones de inicialización
 void initleds(void);
 void initPWM(void);
 void inittemp(void);
 void initServo(void);
-//      Prototipos de funciones
 
 //  Función para configurar display de 7 segmentos
 void configDisplay7(void);
@@ -55,6 +57,7 @@ void desplegarNumero(uint8_t numero);
 
 //  Desplegar punto (1 desplegar, 0 no desplegar)
 void desplegarPunto(uint8_t punto);
+
 
 
 #endif // __INIT_H__
